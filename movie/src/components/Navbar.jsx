9b3f6dd732eb82c7c826e5f2d675aaf165d40dba
Bruno Santana <104157394+devbrunosantana1@users.noreply.github.com>
2022-12-15 17:59:33 -0300

    style Navbar

--- movie/src/components/Navbar.jsx
+++ movie/src/components/Navbar.jsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom"
 import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi"
 
+import './Navbar.css'
+
 export function Navbar() {
     return (
-        <nav className='navbar'>
+        <nav className="navbar">
         <h2>
             <Link to="/"><BiCameraMovie />Movie</Link>
         </h2>

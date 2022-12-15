5ff6eab7a36a68791b5908420a8c3c536c06af48
Bruno Santana <104157394+devbrunosantana1@users.noreply.github.com>
2022-12-15 00:34:15 -0300

    first commit

---
+++ movie/src/main.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App />}>
+          <Route patch="/" element={<Home/>}/>
+          <Route patch="/movie/:id" element={<Movie/>}/>
+          <Route patch="/search" element={<Search/>}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
+)
